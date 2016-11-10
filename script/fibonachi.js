9b18908915d10274815a93e13030b3f986e7233b
Andriy Tomkevych <91atom@gmail.com>
2016-11-10 11:00:21 +0000

    sd

--- script/fibonachi.js
+++ script/fibonachi.js
@@ -1,11 +1,39 @@
-var n = Number(prompt('Input "n" for: Fibonachi'));
-var i = 0;
-var m = n+1;
-document.write(n +'<br>');
-while (i <=10) {
-    var s = n + m;
-    n=m;
-    document.write(n +'<br>');
-    m=s;
-i++;    
+//           1 No Fibonachi
+
+
+// var n = Number(prompt('Input "n" for: fibonachi'));
+// var i = 0;
+// var m = n+1;
+// document.write(n +'<br>');
+// while (i <=10) {
+//     var s = n + m;
+//     n=m;
+//     document.write(n +'<br>');
+//     m=s;
+// i++;    
+// }
+
+
+//                 2 func Fibonachi
+ 
+var n = Number(prompt('Input "n" for: fibonachi'));
+function fibonachi(n) {
+    if (n < 3) {
+        if (n == 0) {
+            return 0;
+        }
+        return 1;
+    }
+    else {
+        return fibonachi(n - 1) + fibonachi(n - 2);
+    }
 }
+for (n; n < 20; n++) {
+    document.write('n = ' + fibonachi(n) + '<br>');
+}
+
+
+
+
+
+
